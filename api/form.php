0041dfdd513878950e950d1fe0c46d1e2bff1b56
samouchka1 <mchio421@protonmail.com>
2023-02-21 23:37:26 -0500

    added validation and styles edit

--- api/form.php
+++ api/form.php
@@ -6,6 +6,13 @@ $request_body = file_get_contents('php://input');
 $data = json_decode($request_body);
 $email = $data->email;
 
+// Validate email
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  $response = array('success' => false, 'message' => 'Invalid email address');
+  echo json_encode($response);
+  exit;
+}
+
 // Insert email into database
 $sql = "INSERT INTO emails (email) VALUES ('$email')";
 
